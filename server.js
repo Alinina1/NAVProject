8c27cedcdbd153f292e750b517b697c66d60210a
Nutfullina Alina <xxxnut@yandex.ru>
2021-10-25 16:32:37 +0500

    pages

--- server.js
+++ server.js
@@ -1,45 +1,30 @@
-const http = require('http');
 const express = require('express');
+//const bodyParser = require('body-parser');//для post-запроса
 
 const app = express();
+//const urlencodedParser = bodyParser.urlencoded({ extended: false }); //для post-запроса
 
 app.set('view engine', 'ejs');
 
-//app.use('/public', express.static('public'));
-//app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname +'/public/'));//тут подключается папка типа где стили статическая
-//покажи папку
-//тут только апка вроде длжна быть дальшейший пусть в другом месте
-//стой явозможно поняла
+app.use(express.static(__dirname +'/public/'));
 
 app.get('/', function (req, res){
-  res.render('shblon');//вызыввется shblon
+  res.render('home');
 })
 
+app.get('/home', function (req, res){
+  res.render('home');
+})
 
-
-app.listen(3000);
-
-//а можещь вернуть чтобы работало с моим портом
-
-/*const server = http.createServer(function(req, res){
-    console.log('URL страницы ' + req.url);
-    if (req.url === '/' || req.url === '/index'){
-      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-      fs.createReadStream(__dirname + '/index.html').pipe(res);
-    }
-    else if (req.url === '/about'){
-      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-      fs.createReadStream(__dirname + '/about.html').pipe(res);
-    }
-    else{
-      res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
-      fs.createReadStream(__dirname + '/404.html').pipe(res);
-    }
-    
-});
-
-server.listen(3000, '192.168.1.126');
-console.log("Мы отслеживаем порт 3000");
-
+app.get('/about', function (req, res){
+  res.render('about');
+})
+//post-запрос
+/*
+app.post('/home',urlencodedParser, function (req, res){
+  if(!req.body) return res.sendStatus(400);
+  console.log(req.body);
+  res.render('home');
+}) 
 */
+app.listen(3000, () => console.log('Server is running'));
