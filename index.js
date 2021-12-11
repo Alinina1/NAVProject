4ff7d588e4c69b1a94f2a7aa1cb49ba2d580a328
Nutfullina Alina <xxxnut@yandex.ru>
2021-12-11 13:49:41 +0500

    feature: add mongoose package

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
 const express = require('express');
-//const bodyParser = require('body-parser');//для post-запроса
+const mongoose = require('mongoose');//база данных mongo db
 const helmet = require('helmet');
 const compression = require('compression');
 const app = express();
-//const urlencodedParser = bodyParser.urlencoded({ extended: false }); //для post-запроса
+
+//routes
+const dictionaryRoutes = require('./routes/dictionary');
+const photoalbumRoutes = require('./routes/photoalbum');
+const meritsRoutes = require('./routes/merits');
+const aboutRoutes = require('./routes/about');
+const homeRoutes = require('./routes/home');
 
 app.set('view engine', 'ejs');
-app.set('port', (process.env.PORT || 3000));
 
 app.use(helmet());
 app.use(compression());
 app.use(express.static(__dirname +'/public/'));
 
-app.get('/', function (req, res){
-  res.render('home');
-})
+app.use('/', homeRoutes);
+app.use('/home', homeRoutes);
+app.use('/about', aboutRoutes);
+app.use('/merits', meritsRoutes);
+app.use('/photoalbum', photoalbumRoutes);
+app.use('/dictionary', dictionaryRoutes);
 
-app.get('/home', function (req, res){
-  res.render('home');
-})
+async function start(){
+    try {
+        const password = "6wBTWeWDZTWbXE9";
+        const url = "mongodb+srv://alinina:6wBTWeWDZTWbXE9@cluster0.qqzdi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        await mongoose.connect(url, {useNewUrlParser: true});
 
-app.get('/about', function (req, res){
-  res.render('about');
-})
+        app.set('port', (process.env.PORT || 3000));
+        app.listen(app.get('port'), () => console.log('Server is running'));
+    } catch (e){
+        console.log(e);
+    }
+}
 
-app.get('/merits', function (req, res){
-  res.render('merits');
-})
+start();
 
-app.get('/photoalbum', function (req, res){
-  res.render('photoalbum');
-})
-
-app.get('/dictionary', function (req, res){
-  res.render('dictionary');
-})
-//post-запрос
-/*
-app.post('/home',urlencodedParser, function (req, res){
-  if(!req.body) return res.sendStatus(400);
-  console.log(req.body);
-  res.render('home');
-}) 
-*/
-app.listen(app.get('port'), () => console.log('Server is running'));
