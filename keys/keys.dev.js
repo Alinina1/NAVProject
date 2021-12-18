c47394589895b4aff962bf6f56c3f8659361eb15
Nutfullina Alina <xxxnut@yandex.ru>
2021-12-18 16:53:48 +0500

    version

---
+++ keys/keys.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB_URI: "mongodb+srv://alinina:6wBTWeWDZTWbXE9@cluster0.qqzdi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    SESSION_SECRET: "some secret value"
+}
